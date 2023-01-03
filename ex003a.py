6dbbfe37c33b61987eecab1d147eb8534c743bd2
Weliton-Silva <welitonjosedasilva@gmail.com>
2023-01-03 14:55:47 -0300

    exercicios curso em video

---
+++ ex003a.py
@@ -0,0 +1,8 @@
+import math
+grau = int(input('Digite o grau: '))
+cos = math.cos(grau)
+seno = math.sin(grau)
+tan = math.tan(grau)
+print(' com o grau sendo {} o cosseno é {:.3f} seno {:.3f} e a tangente {:.3f}'.format(grau, cos, seno, tan))
+
+
---
+++ ex004a.py
@@ -0,0 +1,9 @@
+
+import random
+print(' Sorteio de alunos ')
+n1 = input('Digite nome do primeiro aluno: ')
+n2 = input('Segundo aluno: ')
+n3 = input('Terceiro aluno: ')
+n4 = input('Quarto aluno: ')
+nomes = n1, n2, n3, n4
+print(' O sorteado foi {}'.format(random.choice(nomes)))
