56edce7f83ecab9331fdd9d46b34d9d4ff677e3d
Weliton-Silva <welitonjosedasilva@gmail.com>
2023-01-04 13:44:38 -0300

    exercicios curso em video

--- Funções Trigonométricas_ex002a.py
+++ Funções Trigonométricas_ex002a.py
@@ -1,6 +1,6 @@
 import math
-cat = int(input('Digite o valor do cateto oposto: '))
-adj = int(input('Digite o valor do cateto adjacente: '))
+cat = float(input('Digite o valor do cateto oposto: '))
+adj = float(input('Digite o valor do cateto adjacente: '))
 hipo = math.hypot(cat, adj)
-print('O comprimento  do cateto oposto é {} e o cateto adjacente é {} a hipotenusa é {}'.format(cat, adj, hipo))
+print('O comprimento  do cateto oposto é {} e o cateto adjacente é {} a hipotenusa é {:.2f}'.format(cat, adj, hipo))
 
