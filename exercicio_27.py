506a80c6d9ed58ff994011691ea3996986d5d5d6
Weliton-Silva <welitonjosedasilva@gmail.com>
2023-01-07 03:06:42 -0300

    exercicios curso em video

--- exercicio_27.py
+++ exercicio_27.py
@@ -1,3 +1,3 @@
 nome = input(' Digite seu nome completo: ')
 nome1 = nome.split()
-print('Primeiro nome é {}\nUltimo nome {}'.format(nome1[0], nome1[-1]))
+print('Primeiro nome é: {}\nUltimo nome é: {}'.format(nome1[0], nome1[-1]))
