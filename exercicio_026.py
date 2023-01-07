f7071f00ee7e107146fedc368a76d682c982bb2f
Weliton-Silva <welitonjosedasilva@gmail.com>
2023-01-07 02:23:01 -0300

    exercicios curso em video

--- exercicio_026.py
+++ exercicio_026.py
@@ -1,8 +1,9 @@
 nome = input('Digite uma Frase: ')
 nome1 = nome.upper()
 print(' a Letra "A" na sua frase aparece {} vezes'.format(nome1.count('A')))
-nome2 = nome1.split()
-print(nome2.count('A'))
+print('A Primeira vez que aparece é {} e a ultima vez {}'.format(nome1.find('A'), nome1.rfind('A')))
+
+
 
 
 
